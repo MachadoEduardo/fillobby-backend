dde7fc079d836a25e51486ae81f6144635fc1ab1
MachadoEduardo <05eduardomachado@gmail.com>
2026-09-21 16:26:43 -0300

    chore: configuração ESLINT

---
+++ eslint.config.js
@@ -0,0 +1,32 @@
+import js from '@eslint/js';
+import { defineConfig } from 'eslint/config';
+import globals from 'globals';
+
+export default defineConfig([
+  {
+    ignores: ['node_modules/**', 'coverage/**'],
+  },
+  js.configs.recommended,
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: globals.nodeBuiltin,
+    },
+    rules: {
+      eqeqeq: ['error', 'always'],
+      'no-duplicate-imports': 'error',
+      'no-unused-vars': [
+        'error',
+        {
+          args: 'none',
+          caughtErrors: 'none',
+          ignoreRestSiblings: true,
+        },
+      ],
+      'no-var': 'error',
+      'prefer-const': 'error',
+    },
+  },
+]);
